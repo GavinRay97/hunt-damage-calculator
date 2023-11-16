85f5819ca13067c41e6b05f291d9d51c867a50e0
Gavin Ray <ray.gavin97@gmail.com>
2023-11-16 08:58:36 -0500

    Update vite config for GH pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set base path for GitHub Pages
+  base: '/hunt-damage-calculator/',
   plugins: [react()],
 })
